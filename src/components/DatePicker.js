8ba89412a963163539e4ca6acdb4c8e25d52337b
maevanapcontact <maevanap.contact@gmail.com>
2021-04-11 20:57:28 +0200

    feat: add basic structure DatePicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./DatePicker.scss";
@@ -6,16 +6,83 @@ import "./DatePicker.scss";
 const DatePicker = (props) => {
   const { pickerLabel, pickerName, pickerValue, handlePickerChange } = props;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(pickerName);
+  console.log(handlePickerChange);
+
   return (
-    <label className="datePicker">
-      {pickerLabel}
-      <input
-        type="date"
-        name={pickerName}
-        value={pickerValue}
-        onChange={handlePickerChange}
-      />
-    </label>
+    <div className="picker-wrapper">
+      <label htmlFor="" className="picker-label">
+        {pickerLabel}
+      </label>
+      <span className="picker" id="picker" onClick={() => setIsOpen(!isOpen)}>
+        {pickerValue}
+      </span>
+      <div className={isOpen ? "picker-elt active" : "picker-elt"}>
+        <header className="picker-elt-header"></header>
+        <table className="picker-elt-table">
+          <thead>
+            <tr>
+              <th className="picker-elt-table-header-cell">Sun</th>
+              <th className="picker-elt-table-header-cell">Mon</th>
+              <th className="picker-elt-table-header-cell">Tue</th>
+              <th className="picker-elt-table-header-cell">Wed</th>
+              <th className="picker-elt-table-header-cell">Thu</th>
+              <th className="picker-elt-table-header-cell">Fri</th>
+              <th className="picker-elt-table-header-cell">Sat</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+            </tr>
+            <tr>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+            </tr>
+            <tr>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+            </tr>
+            <tr>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+            </tr>
+            <tr>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+              <td className="picker-elt-table-cell"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
